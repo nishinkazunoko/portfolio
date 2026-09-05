623f5baf2538412724aefb00063038d9fc52fc6a
“Kazuna” <kazunanishi@gmail.com>
2026-09-05 11:25:52 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import heroImg from './assets/images/hero.png'
+import reactLogo from './assets/images/react.svg'
+import viteLogo from './assets/images/vite.svg'
+import './App.css'
+import './assets/scss/style.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <main>
+        <p>portfolioを作ります</p>
+      </main>
+    </>
+  )
+}
+
+export default App
